e5257540a0708e118178fbfff751bf831b4636bd
Asad Ali <[asad.ar451@gmail.com]>
2025-01-29 16:39:51 +0500

    Initialize Angular project structure with routing, components, services, and global styles

---
+++ Angular-Dynamic-Component/src/app/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { UserService } from '../Services/user.service';
+import { User } from '../Models/User';
+
+@Component({
+  selector: 'app-users',
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+  constructor(private userService: UserService) {
+
+  }
+
+  users: User[] = [];
+
+  ngOnInit() {
+    this.users = this.userService.users;
+  }
+}
