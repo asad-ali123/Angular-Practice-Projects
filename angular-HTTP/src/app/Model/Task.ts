769a309a4c899829a8820819b368adf93467f0a0
Asad Ali <[asad.ar451@gmail.com]>
2025-02-03 11:55:09 +0500

    add task ID, implement fetch and delete functionality

--- angular-HTTP/src/app/Model/Task.ts
+++ angular-HTTP/src/app/Model/Task.ts
@@ -1,8 +1,9 @@
 export class Task {
-    constructor(public title: string, 
-        public desc: string, 
-        public assignedTo: string, 
-        public createdAt: string, 
-        public priority: string, 
-        public status: string) { }
+    constructor(public title: string,
+        public desc: string,
+        public assignedTo: string,
+        public createdAt: string,
+        public priority: string,
+        public status: string,
+        public id?: string) { }
 }
