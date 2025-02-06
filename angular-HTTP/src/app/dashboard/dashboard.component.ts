97ac8d32190a0b842321d79b82af074f6ee826f8
Asad Ali <[asad.ar451@gmail.com]>
2025-02-06 20:44:17 +0500

    Add status component to dashboard and update routing

--- angular-HTTP/src/app/dashboard/dashboard.component.ts
+++ angular-HTTP/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+
 import { OverviewComponent } from "./overview/overview.component";
+import { StatusComponent } from "./status/status.component";
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [OverviewComponent],
+  imports: [OverviewComponent, StatusComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
- 
+
 
 }
