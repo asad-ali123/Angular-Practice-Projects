aa5787f3eaf5f03560c4401e986078296fdc3ee3
Asad Ali <[asad.ar451@gmail.com]>
2025-02-08 17:22:27 +0500

    Add routing and update app component for navigation

--- angular-ask-AI/src/app/app.component.ts
+++ angular-ask-AI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidemenuComponent } from "./components/sidemenu/sidemenu.component";
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [SidemenuComponent],
+  imports: [SidemenuComponent , RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-ask-AI/src/app/app.routes.ts
+++ angular-ask-AI/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ImageGenComponent } from './pages/image-gen/image-gen.component';
+import { CricketComponent } from './pages/cricket/cricket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'chat', pathMatch: 'full'
+    },
+    {
+        path: 'chat', component: ChatComponent
+    },
+    {
+        path: 'imagegen', component: ImageGenComponent
+    },
+    {
+        path: 'cricket', component: CricketComponent
+    }
+];
