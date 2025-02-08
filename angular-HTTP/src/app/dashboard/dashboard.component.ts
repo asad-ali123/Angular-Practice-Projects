71a9e8de5bc9288279e7f25e7d4e42ea9a8a851b
Asad Ali <[asad.ar451@gmail.com]>
2025-02-08 10:15:17 +0500

    Refactor dashboard routing and remove overview component

--- angular-HTTP/src/app/dashboard/dashboard.component.ts
+++ angular-HTTP/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,136 @@
-import { Component } from '@angular/core';
-
-import { OverviewComponent } from "./overview/overview.component";
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../Services/task.service';
+import { Task } from '../Model/Task';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CreateTaskComponent } from "./create-task/create-task.component";
+import { TeskDetailComponent } from "./tesk-detail/tesk-detail.component";
+import { SnackbarComponent } from "../utility/snackbar/snackbar.component";
+import { LoaderComponent } from "../utility/loader/loader.component";
 import { StatusComponent } from "./status/status.component";
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [OverviewComponent, StatusComponent],
+  imports: [CommonModule, CreateTaskComponent, TeskDetailComponent, SnackbarComponent, LoaderComponent, StatusComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+   taskService: TaskService = inject(TaskService);
+
+    allTasks: Task[] = [];
+    selectedTask: any;
+    showCreateTaskForm: boolean = false;
+    showDetail: boolean = false;
+    editMode: boolean = false;
+    currentTaskId: string | undefined;
+    isLoading: boolean = false;
+    errorMessage!: string | null;
+    errorSub!: Subscription;
+    currentTaskDetails: Task | null = null
+  
+    ngOnInit(): void {
+      this.fetchAllTasks();
+      this.errorSub = this.taskService.errorSubject.subscribe({
+        next: (httmError) => {
+          this.setErrorMessage(httmError)
+        }
+      })
+  
+    }
+  
+    ngOnDestroy() {
+      this.errorSub.unsubscribe();
+    }
+  
+  
+    OpenCreateTaskForm() {
+      this.showCreateTaskForm = true;
+    }
+    CloseCreateTaskForm() {
+      this.showCreateTaskForm = false;
+      this.editMode = false;
+    }
+    // Show task details component
+  
+    onShowDetail(id: string | undefined) {
+      this.showDetail = true;
+      this.taskService.getTaskDetails(id).subscribe((data) => {
+        let currentTask: Task = data as Task;
+        this.currentTaskDetails = currentTask
+  
+      })
+  
+    }
+    onCloseDetail() {
+      this.showDetail = false;
+    }
+  
+  
+    // HTTP Methods
+    createOrUpdateTask(data: Task) {
+      if (!this.editMode) {
+        this.taskService.CreateTask(data);
+      } else {
+        this.taskService.UpdateTask(this.currentTaskId, data)
+      }
+    }
+  
+    private fetchAllTasks() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 3000);
+      this.taskService.FetchAllTasks().subscribe({
+        next: (tasks) => {
+          this.allTasks = tasks;
+          this.isLoading = false;
+        },
+        error: (error) => {
+          this.setErrorMessage(error)
+        }
+      })
+    }
+  
+    private setErrorMessage(err: HttpErrorResponse) {
+      if (err.error.error === 'Permission denied') {
+        this.errorMessage = "You do not have permisson to perform this action"
+      } else {
+        this.errorMessage = err.message
+      }
+  
+  
+      setTimeout(() => {
+        this.errorMessage = null
+      }, 3000);
+    }
+  
+    fetchAllTasksClicked() {
+      this.fetchAllTasks()
+    }
+  
+    deleteTask(id: string | undefined) {
+      this.taskService.DeleteTask(id);
+  
+  
+    }
+  
+    deletAllTasks() {
+      this.taskService.DeleteAllTasks();
+  
+    }
+  
+    onEditClicked(id: string | undefined) {
+      this.currentTaskId = id;
+      this.showCreateTaskForm = true;
+      this.editMode = true;
+      this.selectedTask = this.allTasks.find((task) => task.id === id)
+    }
+  
+  }
+  
 
 
-}
+
