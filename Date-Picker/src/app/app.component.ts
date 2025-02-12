7ee24796a32ed486002541d4756200a25664526b
Asad Ali <[asad.ar451@gmail.com]>
2025-02-12 23:34:05 +0500

    Refactor Date Picker component to use reactive forms: update template and implement form control for date selection

--- Date-Picker/src/app/app.component.ts
+++ Date-Picker/src/app/app.component.ts
@@ -1,25 +1,56 @@
 // src/app/app.component.ts
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {  IMyDpOptions } from 'mydatepicker';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // Import FormsModule and MyDatePickerModule for template-driven forms and the date picker component.
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule , CommonModule , ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  // DatePicker configuration options: using the format 'dd.mm.yyyy'
   public myDatePickerOptions: IMyDpOptions = {
+    // other options...
     dateFormat: 'dd.mm.yyyy',
-    // You can add additional options as needed.
   };
 
-  // The model that holds the date value. Initialized to October 9, 2018.
-  public model: any = { date: { year: 2018, month: 10, day: 9 } };
+  public myForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      // Empty string or null means no initial value. Can be also specific date for
+      // example: {date: {year: 2018, month: 10, day: 9}} which sets this date to initial
+      // value.
+
+      myDate: [null, Validators.required]
+      // other controls are here...
+    });
+  }
+
+  setDate(): void {
+    // Set today date using the patchValue function
+    let date = new Date();
+    this.myForm.patchValue({
+      myDate: {
+        date: {
+          year: date.getFullYear(),
+          month: date.getMonth() + 1,
+          day: date.getDate()
+        }
+      }
+    });
+  }
+
+  clearDate(): void {
+    // Clear the date using the patchValue function
+    this.myForm.patchValue({ myDate: null });
+  }
 }
